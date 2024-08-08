09450851ca201cc855b2bcea8d750c7a7ed8e609
ghd2408 <ghd2408@gmail.com>
2024-08-08 17:58:00 +0900

    java

---
+++ ch03/js/calc.js
@@ -0,0 +1,16 @@
+function plus(a, b){
+    return a + b;
+}
+
+function minus(a, b){
+    return a - b;
+}
+
+function multi(a, b){
+    return a * b;
+}
+
+function div(a, b){
+    return a / b;
+}
+
